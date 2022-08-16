71e25a1a4cda974b9cb4a59a3266b221aaab9f6c
1camilo5 <cristianc.albarracinf@gmail.com>
2022-08-16 17:48:14 -0500

    segunda modificacion

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main {
+
+    public static void adivinador(){
+        Scanner sc = new Scanner(System.in);
+        int aleatorio1 = (int)(Math.random()*10+1);
+        System.out.println(aleatorio1);
+        System.out.println("Ingrese un numero");
+        int usuario = sc.nextInt();
+        if (usuario == aleatorio1){
+            System.out.println("Usted adivino");
+        }else if(usuario > aleatorio1){
+            System.out.println("Numero mayor");
+        }else if(usuario < aleatorio1){
+            System.out.println("Numero menor");
+        }
+    }
+
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
     }
 }
+
